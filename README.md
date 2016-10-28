d53d4e0dc7e8dc92d393560970b68be0d562d900
MrSalixor <kevin.cocchi@gmail.com>
2016-10-28 22:36:52 +0200

    Support for subdirectories

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # About duplicate-files
-This little Python 3 script will help you find duplicate files in a directory on your computer.
+This little Python 3 script will help you find duplicate files in a directory (and its subdirectories) on your computer.
 
 It can also help you delete them automatically if you wish to.
 
@@ -10,9 +10,9 @@ The script proceeds in two steps to know whether two file are the same or not :
 ## Running the script
 In order to run the script, you need to have Python 3 installed.
 
-The script requires at least an argument which is the absolute path of the directory you wish to check in for duplicate files.
+The script requires at least an argument which is the path of the directory you wish to check in for duplicate files. The check for duplicate files will happen recursively within every subdirectories.
 
-You can also pass a second argument that equals "true" or "false" and will tell the script to delete or not the duplicate files it may find.
+You can also pass a second facultative argument that equals ```true``` or ```false``` and will tell the script to delete or not the duplicate files it may find. By default, this argument is set to ```false```. If subdirectories were to be left empty in the process, they are deleted at the end of the script.
 
 ```{r, engine='python'}
 python duplicatefiles.py PATH DELETION=["TRUE", "FALSE"]
--- duplicatefiles.py
+++ duplicatefiles.py
@@ -59,6 +59,7 @@ def CompareHashes(f1_name, f2_name):
 
     return ( f1_sha == f2_sha )
 
+# A simple pager (because there can be a lot of duplicate files)
 def pager(text, num_lines = 25):
     for index, line in enumerate(text.splitlines()):
         if index % num_lines == 0 and index:
@@ -70,129 +71,146 @@ def pager(text, num_lines = 25):
         else:
             print(line)
 
+# A function to remove the empty directories we may have left
+def remEmptyDir(mypath):
+    for root, dirs, files in os.walk(mypath):
+         for name in dirs:
+             fname = os.path.join(root, name)
+             if not os.listdir(fname): # To check whether the dir is empty
+                 os.removedirs(fname)
+                 remEmptyDir(mypath)
+
     # END OF FUNCTIONS #
 
-# Setting up the variables used to run the script
-if len(sys.argv) < 2:
-    errorMessage("The script requires an absolute path to run.")
-    errorMessage("Correct usage : python duplicatefiles.py path delete=[true,false].")
-    exit(1)
+if __name__ == "__main__":
+    # Setting up the variables used to run the script
+    if len(sys.argv) < 2:
+        errorMessage("The script requires a path to run.")
+        errorMessage("Correct usage : python duplicatefiles.py path delete=[true,false].")
+        exit(1)
 
-path = "" # Absolute path to the directory
-delete = False # Sets whether the script will delete the duplicate files or not
+    path = "" # Path to the directory
+    delete = False # Sets whether the script will delete the duplicate files or not
 
-if len(sys.argv) >= 2: # It seems that a path has been specified : let's check it
-    path = sys.argv[1]
-    if not os.path.exists(path): # In case the path does not exist
-        errorMessage("The specified path ("+path+") does not seem to exist.")
-        exit(2)
+    if len(sys.argv) >= 2: # It seems that a path has been specified : let's check it
+        path = sys.argv[1]
+        if not os.path.exists(path): # In case the path does not exist
+            errorMessage("The specified path ("+path+") does not seem to exist.")
+            exit(2)
+        path = os.path.abspath(path)
 
-if len(sys.argv) >= 3: # It seems that a deletion argument has been provided
-    delete = ( sys.argv[2].lower() == "true" )
+    if len(sys.argv) >= 3: # It seems that a deletion argument has been provided
+        delete = ( sys.argv[2].lower() == "true" )
 
-# Let's confirm the user's choice
-print("The script will retrieve the duplicate files at \033[0;33m"+path+"\033[0;39m.")
-print("It will show the list of duplicate files at the end.\n")
+    # Let's confirm the user's choice
+    print("The script will retrieve the duplicate files at \033[0;33m"+path+"\033[0;39m.")
+    print("It will show the list of duplicate files at the end.\n")
 
-if delete:
-    print("You wish to \033[1;7;31mdelete\033[0;39m the duplicate files at : \033[0;33m"+path+"\033[0;39m.")
-    print("The deletion happens at the end of the script and has to be confirmed.")
-else:
-    print("You wish to \033[1;7;32mkeep\033[0;39m the duplicate files at : \033[0;33m"+path+"\033[0;39m.")
-
-check_deletion = input("Proceed (y/n) ? ")
-
-if check_deletion.lower() != "y":
-    message("\nThe script will now exit without doing anything.")
-    exit(3)
-else:
-    informativeMessage("\nStarting the script ...\n")
-
-f = [] # This array stores the names of the files in the directory
-l = [] # This array will store the names of the duplicate files
-buff = [] # This array is a buffer for the loop
-
-# This loop retrieves the names of the files
-for root, dirs, files in os.walk(path):
-    files = [f for f in files if not f[0] == '.']
-    dirs[:] = [d for d in dirs if not d[0] == '.']
-    f.extend(files)
-    break
-
-filesnb = len(f) # Number of files
-totality = ( filesnb*(filesnb-1) ) / 2 # Final number of iterations
-count = 0 # Keeps track of the progress (number of iterations done)
-
-initial_time = time.clock() # Time at which the script begins
-
-for i, file1 in enumerate(f):
-    buff = [] # We reset the buffer at each loop
-
-    for j, file2 in enumerate(f):
-        # Display of the current progress
-        sys.stdout.write("\r\x1b[K"+"\033[1mProgress :\033[0m "+str( "{:.2f}".format( float(count)/totality*100 ) )+"%")
-        sys.stdout.flush()
-
-        if i >= j: # We skip the checks that have already happened
-            continue
-
-        count += 1
-        f1_name = path+file1
-        f2_name = path+file2
-
-        # First check : comparison of the sizes
-        if not CompareSizes(f1_name, f2_name):
-            continue
-
-        # Second check : comparison of the hashes
-        if CompareHashes(f1_name, f2_name):
-            l.append( (file1, file2) )
-            buff.append(file2)
-
-    # We remove the names of duplicate files from the files array
-    for rem in buff:
-        f.remove(rem)
-        filesnb = len(f)
-        totality -= (filesnb-i-1)
-
-print()
-l.sort(key=operator.itemgetter(0), reverse=True) # Just a little bit of sorting
-
-# Display of the names of duplicate files
-dupes_list = ""
-if(len(l) > 0):
-    if(len(l) != 1):
-        confirmationMessage("\nSome duplicate files were found. Here's the full list.")
+    if delete:
+        print("You wish to \033[1;7;31mdelete\033[0;39m the duplicate files at : \033[0;33m"+path+"\033[0;39m.")
+        print("The deletion happens at the end of the script and has to be confirmed.")
     else:
-        confirmationMessage("\nA duplicate file was found.")
+        print("You wish to \033[1;7;32mkeep\033[0;39m the duplicate files at : \033[0;33m"+path+"\033[0;39m.")
 
-    for index, duo in enumerate(l):
-        if(len(l) != 1):
-            dupes_list += "\n\033[1;34mDuplicate file number "+str(index+1)+" :\033[0;39m"
-        dupes_list += "\n - "+duo[0]
-        dupes_list += "\n - "+duo[1]
-    pager(dupes_list, int(_rows))
-    print()
-else:
-    message("\nNo duplicate files were found.")
+    check_deletion = input("Proceed (y/n) ? ")
 
-# Let's be sure about the user's choice
-if delete and len(l) > 0:
-    print("\033[1mReminder :\033[0;39m you wished to \033[1;7;31mdelete\033[0;39m the duplicate files.")
-    errorMessage("THIS ACTION IS NOT REVERSIBLE.")
-    check_deletion = input("Are you sure you want to proceed (y/n) ? ")
-    print()
-
-    # Proceeds to the deletion of duplicate files (is delete is True)
-    if check_deletion.lower() == "y":
-        for fname in l:
-            print("\033[1;31mDeleted\033[0;39m "+path+fname[1])
-            os.remove(path+fname[1])
-        confirmationMessage("\nThe duplicate files were deleted.")
-    else:
+    if check_deletion.lower() != "y":
         message("\nThe script will now exit without doing anything.")
+        exit(3)
+    else:
+        informativeMessage("\nStarting the script ...\n")
 
-final_time = time.clock() # Time at which the script ends
+    f = [] # This array stores the names of the files in the directory
+    l = [] # This array will store the names of the duplicate files
+    buff = [] # This array is a buffer for the loop
 
-# Display of the execution time
-informativeMessage("The script ran in "+str(int(round((final_time - initial_time)*1000)))+"ms.")
+    # Let's retrieve every file recursively
+    for root, dirs, files in os.walk(path):
+        for fil in files:
+            f.append( os.path.join(root, fil) )
+
+    f.sort() # Just a bit of sorting
+    # f.sort(reverse=True) # Just a bit of sorting
+    filesnb = len(f) # Number of files
+    totality = ( filesnb*(filesnb-1) ) / 2 # Final number of iterations
+    count = 0 # Keeps track of the progress (number of iterations done)
+
+    initial_time = time.clock() # Time at which the script begins
+
+    for i, file1 in enumerate(f):
+        buff = [] # We reset the buffer at each loop
+
+        for j, file2 in enumerate(f):
+            # Display of the current progress
+            sys.stdout.write("\r\x1b[K"+"\033[1mProgress :\033[0m "+str( "{:.2f}".format( float(count)/totality*100 ) )+"%")
+            sys.stdout.flush()
+
+            if i >= j: # We skip the checks that have already happened
+                continue
+
+            count += 1
+            f1_name = os.path.abspath( os.path.join(path, file1) )
+            f2_name = os.path.abspath( os.path.join(path, file2) )
+
+            # First check : comparison of the sizes
+            if not CompareSizes(f1_name, f2_name):
+                continue
+
+            # Second check : comparison of the hashes
+            if CompareHashes(f1_name, f2_name):
+                l.append( (file1, file2) )
+                buff.append(file2)
+
+        # We remove the names of duplicate files from the files array
+        for rem in buff:
+            f.remove(rem)
+            filesnb = len(f)
+            totality -= (filesnb-i-1)
+
+    print()
+
+    # Display of the names of duplicate files
+    dupes_list = ""
+    if(len(l) > 0):
+        if(len(l) != 1):
+            confirmationMessage("\nSome duplicate files were found. Here's the full list.")
+        else:
+            confirmationMessage("\nA duplicate file was found.")
+
+        for index, duo in enumerate(l):
+            if(len(l) != 1):
+                dupes_list += "\n\n\033[1;34mDuplicate file number "+str(index+1)+" :\033[0;39m"
+            dupes_list += "\n  The file          \033[33m"+os.path.relpath(duo[1], path)+"\033[39m"
+            dupes_list += "\n  is a duplicate of \033[33m"+os.path.relpath(duo[0], path)+"\033[39m"
+        pager(dupes_list, int(_rows))
+        print()
+    else:
+        message("\nNo duplicate files were found.")
+
+    # Let's be sure about the user's choice
+    if delete and len(l) > 0:
+        print("\033[1mReminder :\033[0;39m you wished to \033[1;7;31mdelete\033[0;39m the duplicate files.")
+        errorMessage("THIS ACTION IS NOT REVERSIBLE.")
+        check_deletion = input("Are you sure you want to proceed (y/n) ? ")
+        print()
+
+        # Proceeds to the deletion of duplicate files (if the user really wishes to delete)
+        if check_deletion.lower() == "y":
+            for fname in l:
+                file_to_remove = os.path.abspath( os.path.join(path, fname[1]) )
+                try:
+                    os.remove(file_to_remove)
+                    print("\033[1;31mDeleted\033[0;39m "+file_to_remove)
+                except OSError:
+                    errorMessage("The file "+file_to_remove+" does not seem to exist any longer ... Skipping it.")
+
+            remEmptyDir(path) # Let's remove empty directories
+            confirmationMessage("\nThe duplicate files were deleted.")
+        # If the user cancels its choice, the script does nothing
+        else:
+            message("The script will now exit without doing anything.")
+
+    final_time = time.clock() # Time at which the script ends
+
+    # Display of the execution time
+    informativeMessage("The script ran in "+str(int(round((final_time - initial_time)*1000)))+"ms.")
